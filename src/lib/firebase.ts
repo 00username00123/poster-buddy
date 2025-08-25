636a9b4632b36235573ef770e17ae73e5c549d3e
Jamie <00jamie00123@gmail.com>
2025-08-25 00:58:09 +0000

    before refresh: Download the React DevTools for a better development exp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,28 @@
+
 'use client';
 
 import { initializeApp, getApp, getApps } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
   projectId: 'posterscript',
   appId: '1:792606330261:web:5e14b903d4a49866cd3a9c',
-  storageBucket: 'posterscript.firebasestorage.app',
+  storageBucket: 'posterscript.appspot.com',
   apiKey: 'AIzaSyB8ug33Ggm6DEO7e1ThGIRPJCKypHOodN4',
   authDomain: 'posterscript.firebaseapp.com',
   measurementId: '',
   messagingSenderId: '792606330261',
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+let app: ReturnType<typeof initializeApp>;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db = getFirestore(app);
 
 export { app, db };
